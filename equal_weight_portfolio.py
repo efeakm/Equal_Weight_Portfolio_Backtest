75e417d9072c3597d053d9ccc62926dd5e2cf74d
efeakm <103101771+efeakm@users.noreply.github.com>
2022-04-06 17:14:56 +0300

    Streamlit Web App

--- equal_weight_portfolio.py
+++ equal_weight_portfolio.py
@@ -6,88 +6,62 @@ import numpy as np
 import pandas_datareader as pdr
 import matplotlib.pyplot as plt
 import time
+import streamlit as st
 plt.style.use('seaborn')
 
+st.header('Equal Weight Portfolio')
 
 
 ###INPUT PARAMETERS
 #========================================================
+number_of_input_tickers_1 = st.slider('Number of stocks for portfolio 1', max_value = 10)
 
-try:
-
-    inputt = pd.read_excel('input.xlsx')
+tickers = []
+for i in range(number_of_input_tickers_1):
+    tickers.append(st.text_input(f'Stock {i+1}'))
     
-    start = pd.to_datetime(inputt.loc[0,'start date'])
-    
-    end = pd.to_datetime(inputt.loc[0,'stop date'])
-    
-    tickers = inputt['stock list']
-    tickers_2 = inputt['comparison list'][inputt['comparison list'].notna()]
-    
-    portfolio_value = inputt.loc[0,'initial amount']
-    
-    REINVESTMENT_OPTION = inputt.loc[0,'reinvestment option'] #percentage or constant
-    
-    REINVEST_PERCENTAGE = inputt.loc[0,'reinvestment threshold']
-    
-    REINVEST_AMOUNT = inputt.loc[0,'reinvestment amount']
-    
-    BELOW_INITIAL_PERCENTAGE = inputt.loc[0,'below initial percentage']
-    
-    WEEKLY_INVESTMENT_AMOUNT = inputt.loc[0,'weekly amount']
-    
-    MONTHLY_INVESTMENT_AMOUNT = inputt.loc[0,'monthly amount']
-
-except:
-    
-    inputt = pd.DataFrame([['MSFT','AMZN','AAPL','FB'],['SPY']]).T
-    inputt.columns = ['stock list','comparison list']
-    
-    
-    inputt.loc[0,'start date'] = pd.to_datetime("2018-01-01").date()
-
-    inputt.loc[0,'stop date'] = pd.to_datetime("2021-01-01").date()
-    
-    
-    inputt.loc[0,'initial amount'] = 100
-    
-    inputt.loc[0,'reinvestment option'] = 'percentage' #percentage or constant
-    
-    inputt.loc[0,'reinvestment threshold'] = 10
-    
-    inputt.loc[0,'reinvestment amount'] = 20
-    
-    inputt.loc[0,'below initial percentage'] = 0
-    
-    inputt.loc[0,'weekly amount'] = 30
-    
-    inputt.loc[0,'monthly amount'] = 0
-    
-    inputt.to_excel('input.xlsx', index= False)
+st.write('Selected Tickers')
+st.write(tickers)
 
 
-###CREATE HISTORICAL PRICE DATASET
-#========================================================
-df = pdr.get_data_yahoo('MSFT', start,end)
-df_1 = pd.DataFrame(df['Adj Close'].copy())
+tickers_2 = ['SPY']
 
 
-for TICKER in tickers:
-    # start = pd.to_datetime("2018-01-01")
-    # end = pd.to_datetime("2030-01-01")
-    df = pdr.get_data_yahoo(TICKER, start,end)
-    
-    df_1 = df_1.join(df['Adj Close'],rsuffix=f'_{TICKER}')
+form = st.form('input_form')
+
+start = form.date_input('Start Date', value = pd.to_datetime('2018-01-01'))
+end = form.date_input('End Date', value = pd.to_datetime('2021-01-01'))
 
 
-df_1 = df_1.drop('Adj Close',axis=1)
-df_1 = df_1.reset_index()
+
+
+portfolio_value = form.number_input('Initial Amount', value = 100.0000,
+                                  help ='please enter initial amount')
+
+REINVESTMENT_OPTION = form.radio('Reinvestment Option', ['percentage','constant'])
+
+if REINVESTMENT_OPTION == 'constant':
+    REINVEST_AMOUNT = form.number_input('Constant reinvestment amount', value = 20.00)
+
+REINVEST_PERCENTAGE = form.number_input('Reinvestment Percentage', value = 10.00)
+
+
+BELOW_INITIAL_PERCENTAGE = 50
+
+WEEKLY_INVESTMENT_AMOUNT = form.number_input('Weekly Investment Amount', value = 10.00)
+
+MONTHLY_INVESTMENT_AMOUNT = form.number_input('Monthly Investment Amount', value = 50.00)
+
+runn = form.form_submit_button('Run backtesting')
+
+
 
 
 
 
 ### MAIN FUNCTION
 #========================================================================
+@st.cache(suppress_st_warning=True)
 def main_func(temp_input, tickers):
     
     
@@ -269,117 +243,154 @@ def main_func(temp_input, tickers):
     return temp
 
 
-temp = main_func(df_1,tickers)
 
+if runn:
 
-
-
-###COMPARISON DATA
-#=========================================================================
-df = pdr.get_data_yahoo('SPY', start,end)
-df_2 = pd.DataFrame(df['Adj Close'].copy())
-no_of_stocks_2 = len(tickers_2)
-
-
-for TICKER in tickers_2:
-    df = pdr.get_data_yahoo(TICKER, start,end)
     
-    df_2 = df_2.join(df['Adj Close'],rsuffix=f'_{TICKER}')
-
-
-df_2 = df_2.drop('Adj Close',axis=1)
-df_2 = df_2.reset_index()
-
-df_2 = main_func(df_2,tickers_2)
-
-
-
-
-###VISUALIZATION
-#==================================================
-plt.style.use('seaborn')
-fig, ax1 = plt.subplots(figsize=(10,7))
-
-
-
-ax1.plot(temp['Date'], temp['Drawdown'], color = '#C70039', alpha = 0.2)
-ax1.fill_between(temp['Date'], temp['Drawdown'], 0, color = '#C70039', alpha = 0.2)
-
-
-ax1.set_ylabel('Drawdown', fontsize = 14)
-
-
-ax2 = ax1.twinx()
-
-ax2.plot(temp['Date'],temp['total_value'], label = 'Main_Portfolio')
-ax2.plot(df_2['Date'],df_2['total_value'], label = 'Comparison')
-
-ax2.legend(loc = 'lower right')
-ax2.set_ylabel('Total Portfolio Value', fontsize = 14)
-
-
-ax2.annotate('%0.2f' % temp.iloc[-2]['total_value'],
-              xy=(1, temp.iloc[-2]['total_value']), xytext=(8, 0), 
-              xycoords=('axes fraction', 'data'), textcoords='offset points')
-
-ax2.annotate('%0.2f' % df_2.iloc[-2]['total_value'],
-              xy=(1, df_2.iloc[-2]['total_value']), xytext=(8, 0), 
-              xycoords=('axes fraction', 'data'), textcoords='offset points')
-
-fig.tight_layout() 
-
-plt.savefig('comparison_plot.png')
-
-
-
-###OUTPUT
-#=======================================================================
-temp = temp.drop(temp.index[-1], axis = 0)
-
-
-#decimal fix
-col_list = (temp.columns[temp.columns.str.contains('change')].to_list()
-            + ['total_value','reinvestment_due_to_loss','total_value_percentage'])
-temp[col_list] = round(temp[col_list],2)
-
-
-#Denominate change in USD
-for ticker in tickers:
-    temp[f'{ticker}_size'] = temp[f'{ticker}_size'] * temp[f'Adj Close_{ticker}']
-
-
-#Change column names
-cols = temp.columns[temp.columns.str.contains('Adj Close')]
+    ###CREATE HISTORICAL PRICE DATASET
+    #========================================================
+    df = pdr.get_data_yahoo('MSFT', start,end)
+    df_1 = pd.DataFrame(df['Adj Close'].copy())
     
-names = [f'Price_{ticker}' for ticker in tickers]
-
-mapp = {}
-for i in range(len(names)):
-    mapp[cols[i]] = names[i]
-
-
-temp = temp.rename(mapp, axis=1)
-
-
-
-
-#Output file
-temp.to_csv('portfolio_allocation.csv', index = False)
-
-
-#Makes a beep sound if the OS is Windows
-try:
-    import winsound
-    winsound.Beep(frequency = 2500, duration = 1250 )
-except:
-    0
-
-
+    
+    for TICKER in tickers:
+        # start = pd.to_datetime("2018-01-01")
+        # end = pd.to_datetime("2030-01-01")
+        df = pdr.get_data_yahoo(TICKER, start,end)
+        
+        df_1 = df_1.join(df['Adj Close'],rsuffix=f'_{TICKER}')
+    
+    
+    df_1 = df_1.drop('Adj Close',axis=1)
+    df_1 = df_1.reset_index()
+    
+    temp = main_func(df_1,tickers)
     
     
     
     
+    ###COMPARISON DATA
+    #=========================================================================
+    df = pdr.get_data_yahoo('SPY', start,end)
+    df_2 = pd.DataFrame(df['Adj Close'].copy())
+    no_of_stocks_2 = len(tickers_2)
     
+    
+    for TICKER in tickers_2:
+        df = pdr.get_data_yahoo(TICKER, start,end)
+        
+        df_2 = df_2.join(df['Adj Close'],rsuffix=f'_{TICKER}')
+    
+    
+    df_2 = df_2.drop('Adj Close',axis=1)
+    df_2 = df_2.reset_index()
+    
+    df_2 = main_func(df_2,tickers_2)
+    
+    
+    
+  
+    
+    
+    ###OUTPUT
+    #=======================================================================
+    temp = temp.drop(temp.index[-1], axis = 0)
+    
+    
+    #decimal fix
+    col_list = (temp.columns[temp.columns.str.contains('change')].to_list()
+                + ['total_value','reinvestment_due_to_loss','total_value_percentage'])
+    temp[col_list] = round(temp[col_list],2)
+    
+    
+    #Denominate change in USD
+    for ticker in tickers:
+        temp[f'{ticker}_size'] = temp[f'{ticker}_size'] * temp[f'Adj Close_{ticker}']
+    
+    
+    #Change column names
+    cols = temp.columns[temp.columns.str.contains('Adj Close')]
+        
+    names = [f'Price_{ticker}' for ticker in tickers]
+    
+    mapp = {}
+    for i in range(len(names)):
+        mapp[cols[i]] = names[i]
+    
+    
+    temp = temp.rename(mapp, axis=1)
+    
+    
+    
+    
+    #Output file
+    st.subheader('Output File')
+    st.write(temp)
+    
+    
+
+
+  
+    ###VISUALIZATION
+    #==================================================
+    plt.style.use('seaborn')
+    fig, ax1 = plt.subplots(figsize=(10,7))
+    
+    
+    
+    ax1.plot(temp['Date'], temp['Drawdown'], color = '#C70039', alpha = 0.2)
+    ax1.fill_between(temp['Date'], temp['Drawdown'], 0, color = '#C70039', alpha = 0.2)
+    
+    
+    ax1.set_ylabel('Drawdown', fontsize = 14)
+    
+    
+    ax2 = ax1.twinx()
+    
+    ax2.plot(temp['Date'],temp['total_value'], label = 'Main_Portfolio')
+    ax2.plot(df_2['Date'],df_2['total_value'], label = 'Comparison')
+    
+    ax2.legend(loc = 'lower right')
+    ax2.set_ylabel('Total Portfolio Value', fontsize = 14)
+    
+    
+    ax2.annotate('%0.2f' % temp.iloc[-2]['total_value'],
+                  xy=(1, temp.iloc[-2]['total_value']), xytext=(8, 0), 
+                  xycoords=('axes fraction', 'data'), textcoords='offset points')
+    
+    ax2.annotate('%0.2f' % df_2.iloc[-2]['total_value'],
+                  xy=(1, df_2.iloc[-2]['total_value']), xytext=(8, 0), 
+                  xycoords=('axes fraction', 'data'), textcoords='offset points')
+    
+    fig.tight_layout() 
+    
+    st.pyplot(fig)
+    fig.savefig('comparison_plot.png')
+    
+
+
+    
+    
+    
+
+
+    
+
+
+
+
+    st.download_button('Save the output data', temp.to_csv(), file_name = 'output.csv')
+
+
+    with open("comparison_plot.png", "rb") as file:
+        btn = st.download_button(
+                label="Download image",
+                data=file,
+                file_name="comparison_plot.png",
+                mime="image/png",
+                )
+     
+
     
     
     
